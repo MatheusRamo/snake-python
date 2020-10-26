ef003ed76b357214356da2fe0fe230ef77fdced0
MatheusRamo <geof.matheus@gmail.com>
2020-10-26 14:15:49 -0300

    add drawing board

--- snake.py
+++ snake.py
@@ -1,60 +1,86 @@
+from random import randint
 import turtle
 
-# creating a window
+# criando a janela
 window = turtle.Screen()
 window.title("Snake Game")
-window.bgcolor("black")
-window.setup(width=600, height=600)
+window.bgcolor("blue")
+window.setup(width=650, height=650)
 
 # snake
-snake = turtle.Turtle()
-snake.color("red")
-snake.shape("square")
-snake.shapesize(2, 2)
-snake.penup()
+tan_snake = 1
 
-# food
+snake = turtle.Turtle()
+snake.penup()
+snake.goto(10,10)
+snake.color("yellow")
+snake.shape("square")
+snake.shapesize(1, tan_snake)
+
+# comida
 food = turtle.Turtle()
-food.goto(-80,0)
-food.color("blue")
-food.shape("square")
-food.speed(0)
-food.shapesize(2, 2)
 food.penup()
+food.speed(0)
+food.goto(-110,10)
+food.color("yellow")
+food.shape("square")
+food.shapesize(1, 1)
+
+# pen
+pen = turtle.Turtle()
+pen.penup()
+pen.goto(-300, 300)
+pen.pen(pencolor="white", fillcolor="orange", pensize=3, speed=10)
+pen.shape("classic")
+pen.pendown()
+
+def desenha_quadrado(pen, n):
+    for i in range(4):
+        pen.forward(n)
+        pen.left(90)
+
+# Desenha o board
+def desenha_board(pen, tamanho):
+    for row in range(tamanho):
+        for column in range(tamanho):
+            pen.begin_fill()
+            pen.goto((-300+column*20), (300-row*20))
+            pen.pendown()
+            desenha_quadrado(pen,20)
+            pen.end_fill()
+        pen.penup()
 
 
 # funções de movimento
 def snake_right():
     x = snake.xcor()
-    x += 5
+    x += 20
     snake.setx(x)
 
 def snake_left():
     x = snake.xcor()
-    x-= 5
+    x-= 20
     snake.setx(x)
 
 def snake_up():
     y = snake.ycor()
-    y+= 5
+    y+= 20
     snake.sety(y)
 
 def snake_down():
     y = snake.ycor()
-    y -= 5
+    y -= 20
     snake.sety(y)
 
 # configurações do teclado
 window.listen()
-window.onkey(snake_down, "s")
-window.onkeypress(snake_right, "Right")
-window.onkeypress(snake_left, "Left")
-window.onkeypress(snake_up, "Up")
-window.onkeypress(snake_down, "Down")
+window.onkey(snake_right, "Right")
+window.onkey(snake_left, "Left")
+window.onkey(snake_up, "Up")
+window.onkey(snake_down, "Down")
 
 
 # Colisão entre snake e food
-
 def overlapping(min_a, max_a, min_b, max_b):
     return min_b <= max_a and min_a <= max_b
 
@@ -65,16 +91,25 @@ def colider(snake, food):
     y_snake = snake.ycor()
     y_food = food.ycor()
     
-    colider_x = overlapping(x_snake, (x_snake+40), x_food, (x_food+40) )
-    colider_y = overlapping(y_snake, (y_snake+40), y_food, (y_food+40) )
+    colider_x = overlapping(x_snake, (x_snake+20), x_food, (x_food+20) )
+    colider_y = overlapping(y_snake, (y_snake+20), y_food, (y_food+20) )
 
     return colider_x and colider_y
 
-# Main game loop
+def gera_x():
+    numero = randint(-15,15)
+    x = 20*numero + 10
+    return x
 
+desenha_board(pen, 1)
+
+
+# Main game loop
 while True:
     window.update()
     if colider(snake, food):
-        food.color("white")
-    else:
-        food.color("blue")
+        food.penup()
+        food.goto(gera_x(), gera_x())
+        tan_snake += 1
+        snake.shapesize(1, tan_snake)
+
